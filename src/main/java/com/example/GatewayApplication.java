930464eb9729b422ffcca269db1249d7fbe4d792
admin <admin@192.168.1.11>
2016-09-01 10:05:04 +0530

    api agteway

---
+++ src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);//
+
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter(){
+		return new ShallowEtagHeaderFilter();
+	}
+}
